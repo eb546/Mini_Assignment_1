cb2e4d060d63f562669fac70e339adf56da34976
eb546 <eb546@kent.ac.uk>
2024-12-07 22:58:21 +0000

    commit

--- Homework_1.c
+++ Homework_1.c
@@ -3,19 +3,26 @@
 
 
 
-void swap(int *a, int *b)
-{
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
+//void swap(int *a, int *b)
+//{
+    //int temp = *a;
+    //*a = *b;
+    //*b = temp;
+//}
+
+void* calloc(size_t num_elements, size_t element_size);
 
 int main() 
 {
-    int x = 5, y = 10;
-    swap(&x, &y); // x is now 10, y is now 5
+    //int x = 5, y = 10;
+    //swap(&x, &y); // x is now 10, y is now 5
 
-    printf("%d, %d", x, y);
+    //printf("%d, %d", x, y);
 
-    return 0;
+    //return 0;
+
+    int *ptr = (int*)calloc(5, sizeof(int));
+
+    printf("%d", *ptr);
+
 }
