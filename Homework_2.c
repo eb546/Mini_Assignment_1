1d50b1f5eb6fd03081be9da8b3f8c72219cbbb1b
eb546 <eb546@kent.ac.uk>
2024-12-11 17:58:17 +0000

    commit

--- Homework_2.c
+++ Homework_2.c
@@ -16,7 +16,8 @@
 // This typedef struct function is for managing jobs.
 typedef struct 
 {
-    int Job_ID; //Using the int function to identifying the job as a variable
+    int Job_ID; //Using the int function to identifying the job as a variable.
+
     int Priority; //Using the int function to prioritise each jobs from low number to high as a variable.
 } Job;
 
@@ -24,9 +25,13 @@ typedef struct
 typedef struct
 {
     Job *Queue; // Set the job queue by dynamically allocated array.
-    sem_t *Mutex; //Set a semaphore function for mutual exclusion when a queue is executing.
+
+    sem_t *Mutex; //Set a semaphore function with mutual exclusion when a queue is executing.
+
     sem_t *Slot; //Set a semaphore function to limit the number of jobs in slots while being processed.
+
     int Job_Count; // Set a counter of jobs in a queue. 
+
 } JobScheduler;
 
 // This void function allpws to use priority to compare jobs from lowest to highest.
@@ -39,118 +44,128 @@ int Compare_Priority(const void *a, const void *b)
 void Initialize_Scheduler(JobScheduler * Scheduler)
 {
     Scheduler->Queue = (Job*)malloc(Max_Jobs * sizeof(Job)); // Set the dynamic memory allocation for job queue by malloc().
+    
     Scheduler->Job_Count = 0; // Executing the job count to 0.
 
     Scheduler->Mutex = sem_open("/Mutex", O_CREAT, 0666, 1); // Set to execute a semaphore with mutual exclusion function prevents race conditions during the job queue.
+    
     Scheduler->Slot = sem_open("/Slot", O_CREAT, 0666, Max_Jobs); // Set to execute a semaphore with slot function to control the jobs whilst being limit of maximum of jobs.
 
     // Set to check if the semaphore function fails.
     if (Scheduler->Mutex == SEM_FAILED || Scheduler->Slot == SEM_FAILED)
     {
-        perror("Status: Semaphore failed");
-        exit(1);
+        perror("Status: Semaphore failed"); // Print an status message if the program fails along with a decription of the error. 
+
+        exit(1); // Terminating the program. 
     }
 }
 
+// This void function allows to clean up the scheduler and semaphores.
 void Cleanup_Scheduler(JobScheduler * Scheduler)
 {
-    free(Scheduler->Queue);
-    sem_close(Scheduler->Mutex);
-    sem_close(Scheduler->Slot);
-    sem_unlink("/Mutex");
-    sem_unlink("/Slot");
+    free(Scheduler->Queue); // Set to free the dynamic memory allocation for job queue.
+
+    sem_close(Scheduler->Mutex); // Set to close the mutex semaphore. 
+
+    sem_close(Scheduler->Slot); // Set to close the slot semaphore. 
+
+    sem_unlink("/Mutex"); // Set to unlink the mutex semaphore.
+
+    sem_unlink("/Slot"); // Set to unlink job slot semaphore. 
 
 }
 
+// This void function allows to execute the jobs.
 void Execute_Job(int Job_ID, int jobduration, int Pipe_Fd)
 {
-    printf("Job %d is now executing for %d seconds \n", Job_ID, jobduration);
-    sleep(jobduration);
+    printf("Job %d is now executing for %d seconds \n", Job_ID, jobduration); // Prints out the message the which job in the queue is processing in how many seconds.
 
-    write(Pipe_Fd, "Job Completed!", strlen("Job Completed!")+1);
-    close(Pipe_Fd);
+    sleep(jobduration); // Set to sleep or pause the job's execution by the specified duration.
+
+    write(Pipe_Fd, "Job Completed!", strlen("Job Completed!")+1); // Set to write or prints out the completion message to the pipe.
+    
+    close(Pipe_Fd); // Set to close the write function. 
 }
 
+// This void function schedule each jobs based on their priority. 
 void Schedule_Job(JobScheduler * Scheduler, int Job_ID, int Priority, int Pipe_Fd)
 {
-    sem_wait(Scheduler->Slot);
+    sem_wait(Scheduler->Slot); // Set for the job to wait for an available slot. 
 
-    sem_wait(Scheduler->Mutex);
+    sem_wait(Scheduler->Mutex); // Set to lock the job queue to add a new job. 
 
-    Scheduler->Queue[Scheduler->Job_Count].Job_ID = Job_ID;
+    Scheduler->Queue[Scheduler->Job_Count].Job_ID = Job_ID; // Set to add a new job in the queue by its ID.
 
-    Scheduler->Queue[Scheduler->Job_Count].Priority = Priority;
+    Scheduler->Queue[Scheduler->Job_Count].Priority = Priority; // Set to add a new job in the queue by its priority.
 
-    Scheduler->Job_Count++;
+    Scheduler->Job_Count++; // Set to increase the job count until it reaches the maximum amount of jobs in the program.
 
-    qsort(Scheduler->Queue, Scheduler->Job_Count, sizeof(Job), Compare_Priority);
+    qsort(Scheduler->Queue, Scheduler->Job_Count, sizeof(Job), Compare_Priority); // Arranging the jobs based on its priority.
 
-    sem_post(Scheduler->Mutex);
+    sem_post(Scheduler->Mutex); // Release the mutex after the queue is finished executing.
 
-    pid_t pid = fork();
-
-    if (pid == 0)
+    pid_t pid = fork(); // Using the fork() function to execute the job for a child process. 
+    if (pid == 0) 
     {
-        Execute_Job(Job_ID, Job_Duration, Pipe_Fd);
-        exit(0);
+        Execute_Job(Job_ID, Job_Duration, Pipe_Fd); // Executing the job from the child process.
+        exit(0); // Exiting the child process after the job is complete.
     }
 }
 
+// This main function to finalize the job scheduling program system. 
 int main()
 {
-    srand(time(NULL));
+    srand(time(NULL)); // Use the srand() to seed random number generator. 
 
-    int Shm_Id = shmget(IPC_PRIVATE, sizeof(JobScheduler), IPC_CREAT | 0666);
-
+    int Shm_Id = shmget(IPC_PRIVATE, sizeof(JobScheduler), IPC_CREAT | 0666); // Set to create a shared memory for the job scheduleer structure. 
     if (Shm_Id == -1)
     {
-        perror("Creating shared memory failed");
-        exit(1);
+        perror("Creating shared memory failed"); // Print an status message if the program fails along with a decription of the error. 
+        exit(1); // Exiting the child process after the job is complete.
     }
 
-    JobScheduler *Scheduler = (JobScheduler *)shmat(Shm_Id, NULL, 0);
 
+    JobScheduler *Scheduler = (JobScheduler *)shmat(Shm_Id, NULL, 0); // Set to attach the shared memory to the process. 
     if (Scheduler == (void *)-1)
     {
-        perror("Attaching shared memory failed");
-        exit(1);
+        perror("Attaching shared memory failed"); // Print a status message if the program fails along with a decription of the error. 
+        exit(1); // Exiting the child process after the job is complete.
     }
 
-    Initialize_Scheduler(Scheduler);
+    Initialize_Scheduler(Scheduler); // Set to initialize the job scheduler program.
 
-    for (int i = 0; i < Max_Jobs; i++)
+    for (int i = 0; i < Max_Jobs; i++) // Using the for loop to process the maximum job interations until it has completed.
     {
-        int Pipe_Fd[2];
+        int Pipe_Fd[2]; // Declares an array to store the file descriptors for the pipe. 
 
-        if (pipe(Pipe_Fd) == -1)
+        if (pipe(Pipe_Fd) == -1) // Returns -1 if it fails to create the pipe.
         {
-            perror("Creating pipe failed");
-
-            exit(1);
+            perror("Creating pipe failed"); // Print a status message if the program fails along with a decription of the error. 
+            exit(1); // Exiting the child process after the job is complete.
         }
 
-        int Priority = rand() % Max_Priority +1;
+        int Priority = rand() % Max_Priority +1; // Set to generate a random job priority. 
 
-        Schedule_Job(Scheduler, i + 1, Priority, Pipe_Fd[1]);
+        Schedule_Job(Scheduler, i + 1, Priority, Pipe_Fd[1]); // Set to schedule and write every new job with the generated priority.
 
-        char buffer[256];
+        char buffer[256]; // Using the char function to describe the job status from the pipe.
     
-        read(Pipe_Fd[0], buffer, sizeof(buffer));
+        read(Pipe_Fd[0], buffer, sizeof(buffer)); // Using the read function to read the completion message from the pipe.
 
-        printf("Manager received: %s for Job %d\n", buffer, i +1);
+        printf("Manager received: %s for Job %d\n", buffer, i +1); // Prints out the message saying that this specific job is complete. 
     
-        close(Pipe_Fd[0]);
+        close(Pipe_Fd[0]); // close the read end of the pipe. 
     }
 
-    for (int i = 0; i < Max_Jobs; i++)
+    for (int i = 0; i < Max_Jobs; i++) // Set an array for all the jobs to execute.
     {
-        wait(NULL);
+        wait(NULL); // Wait for each job process to finish executing. 
     }
 
-    Cleanup_Scheduler(Scheduler);
+    Cleanup_Scheduler(Scheduler); //Clean up the schedule's resources.
     
-    shmctl(Shm_Id, IPC_RMID, NULL);
+    shmctl(Shm_Id, IPC_RMID, NULL); // Remove the shared memory segment.
 
-    return 0;
+    return 0; // Return from the main function
 
 }
